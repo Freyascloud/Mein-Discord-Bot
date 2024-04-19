e7257512194b40fc859795b26a5f8c28818d70a8
Freyascloud <brandonsmail687@gmail.com>
2024-04-19 10:40:50 +0200

    Init commit

---
+++ Source/main.py
@@ -0,0 +1,46 @@
+import discord
+import os
+import asyncio
+from discord import app_commands
+from discord.ext import commands
+
+
+#intents = discord.Intents.all()
+#intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+
+
+
+@bot.event
+async def on_ready():
+    print(f'Der Code ist eingeloggt als = {bot.user}')
+    bot.loop.create_task(status_task())
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+async def status_task():
+    while True:
+        await bot.change_presence(activity=discord.Game('discord.gg/5HKT8vjGyz'), status=discord.Status.online)
+        await asyncio.sleep(5)
+        await bot.change_presence(activity=discord.Game('Programmiert von Freya'), status=discord.Status.online)
+        await asyncio.sleep(5)
+
+
+@bot.tree.command(name="testgreet", description="guckt ob der command funktioniert")
+async def testgreet(interaction: discord.Interaction):    
+    await interaction.response.send_message(f"Was geht {interaction.user.mention}", 
+    ephemeral=True)
+
+@bot.tree.command(name="say")
+@app_commands.describe(args = "What should i say")
+async def say(interaction: discord.Interaction, args: str):
+    await interaction.response.send_message(f"{interaction.user.name} said: `{args}`", ephemeral=True)
+       
+
+
+
+
+bot.run('MTIzMDQ4NDY3OTM2NzQ2MjkzMw.G3uG55.cVzV5MZbPr0S8IjIX_QdzrNTtNObeilyZ2O0w4')
