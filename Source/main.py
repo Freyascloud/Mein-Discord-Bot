ccd183739ed8790fa433f3d77634c160db5620b9
Freyascloud <brandonsmail687@gmail.com>
2024-08-25 20:06:35 +0200

    Initit Commit

--- Source/main.py
+++ Source/main.py
@@ -37,11 +37,20 @@ async def testgreet(interaction: discord.Interaction):
     ephemeral=True)
 
 @bot.tree.command(name="say")
-@app_commands.describe(args = "What should i say")
+@app_commands.describe(args = "Was soll ich wiederholen?")
 async def say(interaction: discord.Interaction, args: str):
-    await interaction.response.send_message(f"{interaction.user.name} said: `{args}`", ephemeral=True)
+    await interaction.response.send_message(f"{interaction.user.name} said: `{args}`", 
+    ephemeral=True)
        
+@bot.tree.command(name="help", description="Zeigt dir Alle Commands die ich so Besitze")
+async def help(interaction: discord.interactions):
+    await interaction.response.send_message(f"Hier sind alle meine Commands: \n\ntestgreet = guckt ob der Bot on ist und reagiert \n\nSay = Wiederholt deine Text denn du in, denn Befehl schreibst",
+    ephemeral=True)
 
+@bot.tree.command(name="safespace", description="Mit diesem Befehl kommst du zu meinem Discord Server") 
+async def safespace(interaction: discord.interactions,):
+    await interaction.response.send_message(f"Heyo {interaction.user.name},\nWenn du meinem schönen Safespace beitretten möchtest dann bist du sehr gerne Willkommen, \nHier Wäre der link zu meinem DC = discord.gg/5HKT8vjGyz \n\n Vielleicht sehen wir uns ja mal dort",
+    ephemeral=True)
 
 
 
